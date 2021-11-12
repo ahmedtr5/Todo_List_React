f5ff9cc65103016202df06d95a03393c6225ee9a
ahmedtr5 <59222844+ahmedtr5@users.noreply.github.com>
2021-11-12 12:26:31 +0100

    update the update method

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 function TodoForm(props) {
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
 
     const inputRef = useRef(null);
 
@@ -25,7 +25,21 @@ function TodoForm(props) {
     };
 
     return ( 
-        <form className = "form-group" onSubmit={handelSubmit} > 
+        <form className = "form-group" onSubmit={handelSubmit} >
+            {props.edit ? (
+            <>
+            <input 
+            type = "text"
+            placeholder = "update a todo"
+            value = {input}
+            name = "text"
+            className = "todo-input edit"
+            onChange={handelChange}
+            ref={inputRef}
+            />
+            <button className="todo-button edit">update todo</button>
+            </>) : (
+            <>
             <input type = "text"
             placeholder = "add a new todo"
             value = { input }
@@ -35,8 +49,13 @@ function TodoForm(props) {
             ref={inputRef}
             />
             <button className="todo-button">add todo</button>
+            </>
+            )}
        </form> 
     );
+
+
+
 }
 
 export default TodoForm;
